acac1b996f9be5c3c0036bb52ccaa6049e0743d4
Amr Almaz <amra0760@hstat.org>
2018-05-13 18:21:12 +0000

    php done

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
+<!--<pre>-->
 <?php
-echo 'Hello World!';
+$db = new mysqli(
+    'localhost',    //Hostname
+    'amra0760',     //UserID
+    '',             //Password
+    'mvp'           //Name of Database
+    );
+    
+// print_r($db);
+
+if ($db->connect_errno != 0){
+    die("Error in DB connection $db->connect_error");
+} else{
+    // printf("Yes, connected \n");
+    $result = $db->query ('SELECT username, password FROM mvp_table');
+    // print_r($result);
+    printf("<table>");
+    printf("<tr><th> My Usernames</th><th>My Passwords</th></tr>");
+    while($row = $result->fetch_assoc()) {
+        printf("<tr><td>%s</td> <td>%s</td></tr>\n", $row['username'], $row['password']);
+    };
+    printf("<table>");
+}
+
+?>
+<!--</pre>-->
